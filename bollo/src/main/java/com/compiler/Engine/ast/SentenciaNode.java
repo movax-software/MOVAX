7e2d1b846053739db86fe9f6d364d0b45be5235e
luisDBotello <luisdanielbotellolopez02@gmail.com>
2026-01-31 10:58:00 -0700

    Remove unused AST nodes and debugging converter classes
    - Deleted ParseTreeToASTConverterDebug.java, PrintNode.java, ProgramaNode.java, ReturnNode.java, SentenciaNode.java, and WhileNode.java as they are no longer needed.
    - Removed associated class files from the target directory.
    - Cleaned up the codebase by eliminating unnecessary classes that were not contributing to the functionality.

--- bollo/src/main/java/com/compiler/Engine/ast/SentenciaNode.java
+++
@@ -1,8 +0,0 @@
-package com.compiler.Engine.ast;
-
-public abstract class SentenciaNode extends ASTNode {
-    public SentenciaNode(int linea, int columna) {
-        super(linea, columna);
-    }
-}
-
